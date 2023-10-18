f92ccc4f42f1e99e10dacc0c3f7d6f9fe1e79712
Abhishek <abhisheksunny463@gmail.com>
2023-10-18 17:38:55 +0530

    work on templates

--- practice_website/practice_website/settings.py
+++ practice_website/practice_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practice.apps.PracticeConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'practice_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
