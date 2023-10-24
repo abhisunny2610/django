acd4ba5491bc048344c9c386bbcbb62ce2fb50f2
Abhishek <abhisheksunny463@gmail.com>
2023-10-24 23:32:04 +0530

    Work on Frontend Templates

--- school_management/dashboard/urls.py
+++ school_management/dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.admin_dashboard, name="admin_dashboard")
+    path("", view=views.admin_dashboard, name="admin_dashboard"),
+    path("admin_student/", view=views.admin_student, name="admin_student"),
+    path("admin_teacher", view=views.admin_teacher, name="admin_teacher")
 ]
--- school_management/dashboard/views.py
+++ school_management/dashboard/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def admin_dashboard(request):
     return render(request, "Admin_Dashboard.html")
+
+def admin_student(request):
+    return render(request, "Admin_Student.html")
+
+def admin_teacher(request):
+    return render(request, "Admin_Teacher.html")
