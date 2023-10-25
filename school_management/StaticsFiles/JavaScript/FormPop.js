2dc82ed20c979a0172d9a452fa1dd2161b310e84
Abhishek <abhisheksunny463@gmail.com>
2023-10-25 23:27:58 +0530

    Work on Frontend Templates

---
+++ school_management/StaticsFiles/JavaScript/FormPop.js
@@ -0,0 +1,32 @@
+// get elements for popup
+
+const popupbtn = document.querySelector('#add_employee');
+
+const popup = document.querySelector('.popup-wrapper');
+
+const popupclose = document.querySelector('.popup-close');
+
+// popup
+
+popupbtn.addEventListener('click',() =>{
+
+    popup.style.display = "block";
+
+
+});
+
+popupclose.addEventListener('click', () => {
+
+    popup.style.display = 'none';
+
+});
+
+popup.addEventListener('click', (e) => {
+
+    if(e.target.className === 'popup-wrapper'){
+
+      popup.style.display = 'none';
+
+    }
+
+});
