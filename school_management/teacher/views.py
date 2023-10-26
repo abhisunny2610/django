12180e9e88aefc3ac39394decca9fb9101d8bff2
Abhishek <abhisheksunny463@gmail.com>
2023-10-27 01:28:30 +0530

    Work on Frontend Templates

--- school_management/teacher/views.py
+++ school_management/teacher/views.py
@@ -7,13 +7,13 @@ def teacher_home(request):
     return render(request, 'Teacher_Home.html')
 
 def add_notice(request):
-    pass
+    return render(request, "Admin_Notice.html")
 
 def teacher_profile(request):
     pass
 
 def answer(request):
-    pass
+    return render(request, "Teacher_answer.html")
 
 def add_question(request):
-    pass
+    return render(request, "Teacher_Add_Question.html")
