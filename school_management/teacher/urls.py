3ab4857d42cf0b13cd92d73ff13ccc2f0823d846
Abhishek <abhisheksunny463@gmail.com>
2023-10-26 23:59:27 +0530

    Work on Frontend Templates

--- school_management/teacher/urls.py
+++ school_management/teacher/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.teacher_home, name="teacher_home")
+    path("", view=views.teacher_home, name="teacher_home"),
+    path("answer/", view=views.answer, name="answer"),
+    path("add_question/", view=views.add_question, name="add_question"),
+    path('add_notice/', view=views.add_notice, name="add_notice"),
+    path('teacher_profile/', view=views.teacher_profile, name="teacher_profile")
 ]
--- school_management/teacher/views.py
+++ school_management/teacher/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 # teacher home
 def teacher_home(request):
     return render(request, 'Teacher_Home.html')
+
+def add_notice(request):
+    pass
+
+def teacher_profile(request):
+    pass
+
+def answer(request):
+    pass
+
+def add_question(request):
+    pass
