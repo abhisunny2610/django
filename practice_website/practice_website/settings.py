241e279eea4f16780d04646ad7f4384b407e76eb
Abhishek <abhisheksunny463@gmail.com>
2023-10-30 14:27:19 +0530

    Learn extend tag and media images

--- practice_website/practice_website/settings.py
+++ practice_website/practice_website/settings.py
@@ -122,6 +122,11 @@ STATICFILES_DIRS = [
     'StaticsFiles/'
 ]
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT='media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
