44e7ab0f32173fb25adbe9f15b5f4238ab2b4e69
Abhishek <abhisheksunny463@gmail.com>
2023-10-31 23:39:03 +0530

    Work on Models

--- school_management/student/models.py
+++ school_management/student/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    Name = models.CharField(max_length=30, default="")
+    Std = models.CharField(max_length=5, default="")
+    Father_name = models.CharField(max_length=30, default="")
+    City = models.CharField(max_length=15,default="", null=True)
+    Contact = models.CharField(max_length=10, default="", null=True)
+    Dob = models.DateField()
+    Image = models.ImageField(upload_to='Image', blank=True, null=True)
+    Registered = models.DateField(auto_now_add=True)
+
+    def register_student(self, Name, Std, Father_name, City, Contact, Dob, Image, Registered):
+        self.Name = Name
+        self.Std = Std
+        self.Father_name = Father_name
+        self.City = City
+        self.Contact = Contact
+        self.Dob = Dob
+        self.Image = Image
+        self.Registered = Registered
+
+        self.save()
+
+    
--- school_management/teacher/models.py
+++ school_management/teacher/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    Name = models.CharField(max_length=30, default="")
+    Subject = models.CharField(max_length=30, default="")
+    Qualification = models.CharField(max_length=10, default="")
+    City = models.CharField(max_length=15,default="", null=True)
+    Contact = models.CharField(max_length=10, default="", null=True)
+    Dob = models.DateField()
+    Salary = models.CharField(max_length=10, null=True)
+    Performance = models.CharField(default="", max_length=10, null=True)
+    Image = models.ImageField(upload_to='Image', blank=True, null=True)
+    Registered = models.DateField(auto_now_add=True)
+
+    def register_student(self, Name, Subject, Qualification,Salary, Performance, City, Contact, Dob, Image, Registered):
+        self.Name = Name
+        self.Subject = Subject
+        self.Qualification = Qualification
+        self.Salary = Salary
+        self.Performance = Performance
+        self.City = City
+        self.Contact = Contact
+        self.Dob = Dob
+        self.Image = Image
+        self.Registered = Registered
+
+        self.save()
