02263657deaee650ad1e1dc8ce41fa97d46c1c8c
Abhishek <abhisheksunny463@gmail.com>
2023-11-03 14:45:50 +0530

    Image Uploader Project

--- image_uploader/image_uploader/settings.py
+++ image_uploader/image_uploader/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'image_uploader_app.apps.ImageUploaderAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'image_uploader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "Templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    'StaticFiles'
+]
+
+MEDIA_URL = 'Media/'
+
+MEDIA_ROOT = BASE_DIR / "Media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
