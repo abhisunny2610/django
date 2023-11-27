2bf426f4d2163e71bcd7d11bbce1ea9f858d09fa
Abhishek <abhisheksunny463@gmail.com>
2023-11-28 00:10:27 +0530

    Resume Builder Project

--- resume_builder_project/main_app/forms.py
+++ resume_builder_project/main_app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 # validators=[RegexValidator(regex='^(\+?\d{1,3})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$')]
 
 
-class PersonDetails(forms.Form):
+class PersonDetailsForm(forms.Form):
 
     # Personal Details Field
     name = forms.CharField(label="Name", max_length=30)
@@ -16,6 +16,12 @@ class PersonDetails(forms.Form):
     skills_2 = forms.CharField(label="Skill 2")
     skills_3 = forms.CharField(required=False, label="Skill 3")
     skills_4 = forms.CharField(required=False, label="Skill 4")
+
+    # Soft Skills 
+    soft_skills_1 = forms.CharField(label="Soft Skill 1")
+    soft_skills_2 = forms.CharField(label="Soft Skill 2")
+    soft_skills_3 = forms.CharField(required=False, label="Soft Skill 3")
+    soft_skills_4 = forms.CharField(required=False, label="Soft Skill 4")
 
     # Experience Field
     experience_1_title = forms.CharField(label="Job Title")
@@ -31,7 +37,19 @@ class PersonDetails(forms.Form):
     education_1=forms.CharField(label="Degree obtained")
     education_1_dur=forms.CharField(label="Duration")
     education_1_name = forms.CharField(label="College Name")
-    education1_score=forms.CharField()
-    education_2=forms.CharField()
-    education_2_dur=forms.CharField()
-    education2_score=forms.CharField()
+    education1_score=forms.CharField(label="Score")
+    education_2=forms.CharField(label="Degree obtained")
+    education_2_dur=forms.CharField(label="Duration")
+    education_2_name = forms.CharField(label="College Name")
+    education2_score=forms.CharField(label="Score")
+
+    # Project Fields
+    project_1 = forms.CharField(label="Project Name")
+    project_1_desc = forms.CharField(label="Project Description", required=False, widget=forms.Textarea)
+    # project_1_url = forms.CharField(label="Project Url", required=False, widget=forms.URLField,)
+    project_2 = forms.CharField(label="Project Name")
+    project_2_desc = forms.CharField(label="Project Description", required=False, widget=forms.Textarea)
+    # project_2_url = forms.CharField(label="Project Url", required=False, widget=forms.URLField)
+    project_3 = forms.CharField(label="Project Name", required=False)
+    project_3_desc = forms.CharField(label="Project Description", required=False, widget=forms.Textarea)
+    # project_3_url = forms.CharField(label="Project Url", required=False, widget=forms.URLField)
--- resume_builder_project/main_app/views.py
+++ resume_builder_project/main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .forms import PersonDetailsForm
 
 # Create your views here.
+
+
 def home(request):
     return render(request, "Pages/Home.html")
+
+
+def details_form(request):
+    form = PersonDetailsForm()
+    if request.method == "GET":
+        return render(request, "Forms/Details_Form.html", {"form": form})
+
+    if request.method == "POST":
+        pass
